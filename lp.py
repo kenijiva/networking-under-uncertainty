e09087030c134ddcc74c86344fa748477d184a4c
Kenan Besic <besick@ethz.ch>
2021-03-03 17:55:14 +0100

    Changes in computation: Capacity update per Gbps and maximum capacity update per fiberhut

--- lp.py
+++ lp.py
@@ -1,7 +1,8 @@
 import networkx as nx
 from pulp import *
+#def find_capacity_update(beta, gamma, topology, paths, demand, scenarios, link_lease_cost, transceiver_cost, n_wavelengths_fiber, wavelength_capacity, added_edges=[], cheapest=None):
 
-def find_capacity_update(beta, gamma, topology, paths, demand, scenarios, link_lease_cost, transceiver_cost, n_wavelengths_fiber, wavelength_capacity, added_edges=[], cheapest=None):
+def find_capacity_update(beta, gamma, topology, paths, demand, scenarios, fiberhut_capacity, gbps_cost, fiberhut_cost, added_edges=[], cheapest=None):
 
     prob = LpProblem(name="Capacity_increase", sense=LpMinimize)
     edges = sorted(topology.edges)
@@ -14,29 +15,24 @@ def find_capacity_update(beta, gamma, topology, paths, demand, scenarios, link_l
     scenarios = sorted(scenarios)
     n_scenarios = len(flat_scenarios)
 
-    #print(capacity)
-    #print(demand)
-    #demand = {k: v/1000. for k,v in demand.items()}
-    #capacity = {k: v/1000. for k,v in capacity.items()}
-    #print('\n'*3)
-    #print(capacity)
-    #print(demand)
-
     #############################################
     ##              LP VARIABLES               ##
     #############################################
+
+    # Scenarios that are successful
     managed = LpVariable.dicts("Managed scenario", range(n_scenarios), cat='Binary')
     allocations = LpVariable.dicts("Allocations", range(n_paths), lowBound=0, cat='Continous')
     #c_update = LpVariable.dicts("capacity update", edges, lowBound=0, cat='Continous')
+    capacity_update = LpVariable.dicts("capacity update", edges, lowBound=0, cat='Continous')
+    fiberhut_update = LpVariable.dicts("fiberhut update", edges, lowBound=0, cat='Integer')
     
-    wavelengths_update = LpVariable.dicts("wavelengths update", edges, lowBound=0, cat='Integer')
-    fibers_update = LpVariable.dicts("fiber update", edges, lowBound=0, cat='Integer')
     for e in added_edges:
-        wavelengths_update[e].lowBound = 1
-        fibers_update[e].lowBound = 1
+        fiberhut_update[e].lowBound = 1
 
     #############################################
-    ##         AVAILABILITY CONSTRAINTS        ##
+    ##    AVAILABILITY CONSTRAINTS             ##
+    ##    for each flow                        ##
+    ##    sum(p_relevant_scenarios) >= beta    ##
     #############################################
     availability_constraints = []
     for flow, flow_scenarios in scenarios:
@@ -44,26 +40,30 @@ def find_capacity_update(beta, gamma, topology, paths, demand, scenarios, link_l
         availability_constraints.append(constr)
 
     #############################################
-    ##          CAPACITY CONSTRAINTS           ##
-    #############################################
-    # map edges to paths
-    edge2path = dict(zip(edges,[[p for p in range(n_paths) if e in paths[p]] for e in edges]))
-    # for each edge
-    # TODO replace capacity[e] with wavelength_befor*wavelenght_capacity
-    capacity_constraints = [lpSum([allocations[ind] for ind in edge2path[e]]) <= capacity[e]+wavelengths_update[e]*wavelength_capacity for e in edges]
-
-    #############################################
-    ##       SATISFACTION CONSTRAINTS          ##
+    ##    SATISFACTION CONSTRAINTS             ##
+    ##    for each flow: each rel. scenario    ##
+    ##    active_allocation >= gamma*demand    ##
     #############################################
     flow2path = dict(zip(flows, [[p for p in range(n_paths) if f[0]==paths[p][0][0] and f[1]==paths[p][-1][1]] for f in flows]))
     satisfaction_constraints = [lpSum([allocations[path] for path in flow2path[f] if set(paths[path]).isdisjoint(set(s))]) >= managed[flat_scenarios.index(sorted(s))]*gamma*demand[f] for f,scens in scenarios for s,_ in scens]
 
     #############################################
-    ##   MAXIMUM CAPACITY UPDATE CONSTRAINTS   ##
+    ##    CAPACITY CONSTRAINTS                 ##
+    ##    for each edge:                       ##
+    ##    sum_allocations_edge <= c + c_up     ##
     #############################################
-    max_capacity_constraints = [capacity[e] + wavelengths_update[e]*wavelength_capacity <= fibers_update[e]*n_wavelengths_fiber*wavelength_capacity for e in edges]
+    # map edges to paths
+    edge2path = dict(zip(edges,[[p for p in range(n_paths) if e in paths[p]] for e in edges]))
+    capacity_constraints = [lpSum([allocations[ind] for ind in edge2path[e]]) <= capacity[e]+capacity_update[e] for e in edges]
 
-    objective = lpSum([2*wavelengths_update[e]*transceiver_cost + fibers_update[e]*link_lease_cost[e] for e in edges])
+    #############################################
+    ##   MAXIMUM CAPACITY UPDATE CONSTRAINTS   ## TODO
+    ##   for each edge:                        ## capacity[e] on both sides
+    ##   c + c_up <= c_max + c_max_update      ##
+    #############################################
+    max_capacity_constraints = [capacity[e] + capacity_update[e] <= capacity[e] +fiberhut_update[e] * fiberhut_capacity for e in edges]
+
+    objective = lpSum([capacity_update[e] * gbps_cost + fiberhut_update[e] * fiberhut_cost for e in edges])
 
     for a in availability_constraints:
         prob += a
@@ -71,9 +71,11 @@ def find_capacity_update(beta, gamma, topology, paths, demand, scenarios, link_l
         prob += a
     for a in satisfaction_constraints:
         prob += a
+    for a in max_capacity_constraints:
+        prob += a
 
     if cheapest != None:
-        prob += lpSum([2*wavelengths_update[e]*transceiver_cost + fibers_update[e]*link_lease_cost[e] for e in edges]) <= cheapest
+        prob += lpSum([capacity_update[e] * gbps_cost + fiberhut_update[e] * fiberhut_cost for e in edges]) <= cheapest
 
     prob += objective
 
@@ -89,6 +91,8 @@ def find_capacity_update(beta, gamma, topology, paths, demand, scenarios, link_l
         #print([(managed[s].value()) for s in range(n_scenarios)])
         #print(len([value(managed[s]) for s in range(n_scenarios)]))
         #print(objective.value())
+        #print({k: capacity_update[k].value() for k in capacity_update})
+        #print({k: fiberhut_update[k].value() for k in fiberhut_update})
         #print(objective.value())
         return objective.value()
     else:
--- main.py
+++ main.py
@@ -14,26 +14,21 @@ import random
 
 from tqdm import tqdm
 
-def update_capacity(beta, gamma, topology, n_paths, demand, link_lease_cost, transceiver_cost, n_wavelengths_fiber, wavelength_capacity, added_edges=[], cheapest=None):
+def update_capacity(beta, gamma, topology, n_paths, demand, fiberhut_capacity, gbps_cost, fiberhut_cost, added_edges=[], cheapest=None):
 
     paths = k_shortest_paths(topology, n_paths)
     scenarios = get_flow_scenarios(topology, paths) # TODO reuse scenarios, not everly flow changes i think
 
-    cost = find_capacity_update(beta, gamma, topology, paths, demand, scenarios, link_lease_cost, transceiver_cost, n_wavelengths_fiber, wavelength_capacity, added_edges=added_edges, cheapest=None)
+    cost = find_capacity_update(beta, gamma, topology, paths, demand, scenarios, fiberhut_capacity, gbps_cost, fiberhut_cost, added_edges=added_edges, cheapest=cheapest)
 
     return cost
 
 
-
-def add_edges(run, beta, gamma, topology, n_paths, demand, link_lease_cost, transceiver_cost, n_wavelengths_fiber, wavelength_capacity, possible_edges, weibull_scale, weibull_shape):
+def add_edges(beta, gamma, topology, n_paths, demand, fiberhut_capacity, gbps_cost, fiberhut_cost, possible_edges, weibull_scale, weibull_shape):
     # calculate the cost of not adding
-    no_adding_cost = update_capacity(beta, gamma, topology, n_paths, demand, link_lease_cost, transceiver_cost, n_wavelengths_fiber, wavelength_capacity)
+    no_adding_cost = update_capacity(beta, gamma, topology, n_paths, demand, fiberhut_capacity, gbps_cost, fiberhut_cost)
     cheapest = no_adding_cost
     cheapest_es = []
-
-    with open(f'{run}.txt', 'a+') as my_run:
-        my_run.write(f'0 {cheapest} {cheapest_es}\n')
-
 
     # Find cheaper topology
     possible_add = [e for e in possible_edges if not e in topology.edges]
@@ -45,12 +40,14 @@ def add_edges(run, beta, gamma, topology, n_paths, demand, link_lease_cost, tran
     for r in range(1,len(possible_add)):
 
         # new_tops list of (added_edges, their_min cost), becomes then topologies
-        new_tops = [(es + [add], cost+link_lease_cost[add]+2*transceiver_cost) for es,cost in topologies for add in possible_add[index_f(possible_add, es):]]
+        new_tops = [(es + [add], r*fiberhut_cost) for es,cost in topologies for add in possible_add[index_f(possible_add, es):]]
 
         new_tops = [(sorted(i),j) for i,j in new_tops if j < cheapest]
 
 
-        max_r = int(cheapest/(3600+2*transceiver_cost))
+        max_r = int(cheapest/(r*fiberhut_cost))
+        print(cheapest, r*fiberhut_cost)
+
         #sum([link_lease_cost[e]+2*transceiver_cost for e in possible_add])
 
         #Consider only the ones that are cheaper
@@ -63,15 +60,13 @@ def add_edges(run, beta, gamma, topology, n_paths, demand, link_lease_cost, tran
                 for e in es:
                     G.add_edge(*e, prob_failure=possible_edge_probabilities[e], capacity=0)
                 #total_cost = update_capacity(beta, gamma, G, cutoff, demand, edge_cost) + cost
-                total_cost = update_capacity(beta, gamma, G, n_paths, demand, link_lease_cost, transceiver_cost, n_wavelengths_fiber, wavelength_capacity, added_edges=es, cheapest = None)
+                total_cost = update_capacity(beta, gamma, G, n_paths, demand, fiberhut_capacity, gbps_cost, fiberhut_cost, added_edges=es, cheapest=cheapest)
 
                 if total_cost < cheapest:
                     cheapest = min(total_cost, cheapest)
                     cheapest_es = es
-                    print(cheapest)
-
-        with open(f'{run}.txt', 'a+') as my_run:
-            my_run.write(f'{r} {cheapest} {cheapest_es}\n')
+                    max_r = int(cheapest/(r*fiberhut_cost))
+                    print(cheapest,max_r)
 
         #filter tops out for next iteration
         new_tops = [(i,j) for i,j in new_tops if j < cheapest]
@@ -84,29 +79,28 @@ for run in range(1):
     
     topology_path = 'topology/B4'
     topology = pd_to_nx(read_topology(topology_path, weibull_scale, weibull_shape))
-    beta = 0.9999
+    beta = 0.999
     gamma = 1.0
     cutoff = 0.0000001
     demand = read_demand(topology_path, topology, 0)
+    #demand = {k: demand[k]*0.7 for k in demand}
     n_paths = 4
     
     
     wavelength_capacity = 400 #Gbps
-    
-    transceiver_gbps_cost = 10 #$ per Gbps
+    n_wavelengths_fiber = 64 # number wavelengths per fiber
+    gbps_cost = 10 #$ per Gbps
     transceiver_amortization_years = 3 # How long can I keep transceiver
-    transceiver_cost = wavelength_capacity*transceiver_gbps_cost/transceiver_amortization_years #ammortized $ per transceiver per year
+    fiber_cost = 3600 # $ per year
+    n_fibers_per_fiberhut = 200 # How many fibers are build together
+
+    #fiber_capacity = n_wavelengths_fiber*wavelength_capacity # Capacity of fiber
+    fiberhut_capacity = n_fibers_per_fiberhut * n_wavelengths_fiber * wavelength_capacity
+    gbps_cost = gbps_cost / transceiver_amortization_years # Cost per Gbps per year
+    fiberhut_cost = n_fibers_per_fiberhut*fiber_cost # Cost per fiber hut
     
-    fiber_lease_cost = 3600 # $ per year
-    n_wavelengths_fiber = 40 # number wavelengths per fiber
+    possible_edges = [(i,j) for i in topology.nodes for j in topology.nodes if i!=j] # TODO: Select some of these
+
+    #################
     
-    possible_edges = [(i,j) for i in topology.nodes for j in topology.nodes if i!=j] # TODO read out from fiber map
-    n_fibers_link = [1 for e in possible_edges] # TODO For each potential edge: number of fibers between nodes (one link may consist of 3 fibers)
-    
-    
-    link_lease_cost = [fiber_lease_cost*i for i in n_fibers_link]
-    link_lease_cost = dict(zip(possible_edges, link_lease_cost))
-    
-    ################
-    
-    add_edges(run, beta, gamma, topology, n_paths, demand, link_lease_cost, transceiver_cost, n_wavelengths_fiber, wavelength_capacity, possible_edges, weibull_scale, weibull_shape)
+    add_edges(beta, gamma, topology, n_paths, demand, fiberhut_capacity, gbps_cost, fiberhut_cost, possible_edges, weibull_scale, weibull_shape)
