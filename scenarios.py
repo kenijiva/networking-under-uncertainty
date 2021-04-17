8e093f8778e306090c0ceb3686cf471793be0f2b
Kenan Besic <besick@ethz.ch>
2021-04-17 12:46:30 +0200

    speed up scenario creation and SRLGs

--- scenarios.py
+++ scenarios.py
@@ -8,46 +8,46 @@ def group_paths_by_flow(paths, flows):
     n_paths=len(paths)
     return list(zip(flows, [[paths[p] for p in range(n_paths) if f[0]==paths[p][0][0] and f[1]==paths[p][-1][1]] for f in flows]))
 
-def get_scenarios(flow2path, topology):
-    res = []
-    lb_scens = set()
-    for i,j in flow2path:
-        all_scens = set()
-        all_edges = set().union(*j)
+def create_scenarios(parent, all_edges, scens, paths, topology):
+    if parent == []:
+        idx = 0
+    else:
+        idx = all_edges.index(parent[-1])+1
 
-        all_smaller_cutoff = False
-        for path_len in range(len(j)):
-            if all_smaller_cutoff:
+    children = []
+    for e in all_edges[idx:]:
+        scen = parent + [e]
+
+        prob = prod([topology[src][tgt]['prob_failure'] for src,tgt in scen]) * prod([1-topology[src][tgt]['prob_failure'] for src, tgt in set(scen)-set(all_edges)])
+
+        if prob < 10**(-5):
+            continue
+
+        infeasible = True
+
+        for p in paths:
+            infeasible = infeasible and bool(set(p) & set(parent + [e, (e[1],e[0])]))
+            if not infeasible:
                 break
 
-            fail_paths_gen = combinations(j,path_len)
-            all_smaller_cutoff = True
-            for fail_paths in fail_paths_gen:
-                scens = set([(frozenset(p)) for p in list(product(*fail_paths))])
-                scens = scens-all_scens
-                scens = set([scen for scen in scens if not any([lb.issubset(scen) for lb in lb_scens])])
-                lb_scens = lb_scens.union(set([scen for scen in scens if all([scen & set(path) for path in j])]))
+        if not infeasible:
+            create_scenarios(parent + [e], all_edges, scens, paths, topology)
+            scens.append(parent+[e])
 
-                scens = scens-all_scens
-                all_scens = all_scens.union(scens)
-                if scens == set():
-                    probs = [0.]
-                else:
-                    probs = [prod([topology[src][tgt]['prob_failure'] for src,tgt in scen])*prod([1-topology[src][tgt]['prob_failure'] for src,tgt in all_edges-scen]) for scen in scens]
-                    
-                all_smaller_cutoff = all_smaller_cutoff and max(probs) <=  10**(-5)
-            all_probs = [prod([topology[src][tgt]['prob_failure'] for src,tgt in scen])*prod([1-topology[src][tgt]['prob_failure'] for src,tgt in all_edges-scen]) for scen in all_scens]
-            all_smaller_cutoff = (sum(all_probs)) >= 0.999
+def get_scenarios(flow2path, topology):
+    all_scens = []
+    for i,j in flow2path:
+        all_edges = list(set().union(*j))
 
-        res.append((i,all_scens))
+        all_edges = list(set([(min(e[0],e[1]), max(e[0],e[1])) for e in all_edges])) # use undirected for failures (1,2) indicates (1,2), (2,1)
 
-    res = [(i, set([scen for scen in all_scens if not any(lb.issubset(scen) for lb in lb_scens)])) for i, all_scens in res]
-
-    return res
+        scens = [[]]
+        create_scenarios([], all_edges, scens, j, topology)
+        all_scens.append((i, scens))
+    return all_scens
+        
 
 def get_flow_scenarios(topology, paths):
-    # TODO flow 1 2 and 2 1 share 100% scenarios:
-    # TODO use only flows i < j. flow2path change edges direction s.t  s < t
     flows = get_flows(topology)
     flows = [(i,j) for i in topology.nodes for j in topology.nodes if i < j]
     flow2path = group_paths_by_flow(paths, flows)
@@ -60,6 +60,8 @@ def get_flow_scenarios(topology, paths):
         probs = [prod([topology[e[0]][e[1]]['prob_failure'] if e in s else 1-topology[e[0]][e[1]]['prob_failure'] for e in j_edges]) for s in j]
         feasible_scenarios[i] = (flow, list(zip([list(x) for x in j],probs)))
 
+    # make failure scenarios from bidirectional link to two unidirection links
+    # (1,2) -> (1,2), (2,1)
     for i, j in feasible_scenarios:
         for k in j:
             for el in k[0].copy():
