cad132f0be2dcb983a8b68e3826a4a3515708873
root <root@besick.vsos.ethz.ch>
2021-04-21 10:33:12 +0200

    add 013 experiment

---
+++ run013.sh
@@ -0,0 +1,15 @@
+#for f in experiments/*/*.cfg
+FILES="
+experiments/013-cost_over_time_capacity_ksp4_forward_upgrade_t5/config.cfg
+experiments/013-cost_over_time_capacity_ksp4_backward_upgrade_t5/config.cfg
+experiments/013-cost_over_time_capacity_pst6_forward_upgrade_t5/config.cfg
+experiments/013-cost_over_time_capacity_pst6_backward_upgrade_t5/config.cfg
+experiments/013-cost_over_time_capacity_ksp4_forward_add_t5/config.cfg
+experiments/013-cost_over_time_capacity_ksp4_backward_add_t5/config.cfg
+experiments/013-cost_over_time_capacity_pst6_forward_add_t5/config.cfg
+experiments/013-cost_over_time_capacity_pst6_backward_add_t5/config.cfg"
+for f in $FILES
+do
+	echo $f
+	python3 main.py -c $f
+done
