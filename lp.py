790c2d2249975cb77a517c416c1383b4158f33a3
Kenan Besic <besick@ethz.ch>
2021-04-24 10:30:52 +0200

    cleanup

--- lp.py
+++ lp.py
@@ -1,9 +1,13 @@
 import networkx as nx
 from pulp import *
-#def find_capacity_update(alpha, topology, paths, demand, scenarios, link_lease_cost, transceiver_cost, n_wavelengths_fiber, wavelength_capacity, added_edges=[], cheapest=None):
 
-def find_capacity_update(alpha, topology, paths, demand, scenarios, fiberduct_capacity, gbps_cost, fiberduct_cost, added_edges=[], cheapest=None, max_capacity_update=None, max_fiberduct_update=None):
+def find_capacity_update(alpha, topology, paths, demand, scenarios,
+        fiberduct_capacity, gbps_cost, fiberduct_cost, added_edges=[],
+        max_capacity_update=None, max_fiberduct_update=None):
+    """Formulation of the MILP to find capacity and fiber duct upgrades.
+    Params and return see main."""
 
+    # Initialize constants
     prob = LpProblem(name="Capacity_increase", sense=LpMinimize)
     edges = sorted(topology.edges)
     nodes = sorted(topology.nodes)
@@ -15,30 +19,36 @@ def find_capacity_update(alpha, topology, paths, demand, scenarios, fiberduct_ca
     flat_scenarios = [sorted(list(k)) for k in flat_scenarios]
     scenarios = sorted(scenarios)
     n_scenarios = len(flat_scenarios)
-    #print(n_scenarios)
 
     #############################################
     ##              LP VARIABLES               ##
     #############################################
 
-    # Scenarios that are successful
+    # Scenarios that are handled
     managed = LpVariable.dicts("Managed scenario", range(n_scenarios), cat='Binary')
+    # Allocation vector
     allocations = LpVariable.dicts("Allocations", range(n_paths), lowBound=0, cat='Continous')
+    # Capacity upgrades
     capacity_update = LpVariable.dicts("capacity update", edges, lowBound=0, cat='Continous')
+    # Fiber duct upgrades
     fiberduct_update = LpVariable.dicts("fiberduct update", edges, lowBound=0, cat='Integer')
     
+    # Limit on maximum fiber duct upgrade
     if max_fiberduct_update != None:
         for k in max_fiberduct_update:
             if k in edges:
                 fiberduct_update[k].upBound = max_fiberduct_update[k]
+    # Limit on maximum capacity upgrade
     if max_capacity_update != None:
         for k in max_capacity_update:
             if k in edges:
                 capacity_update[k].upBound = max_capacity_update[k]
 
+    # Enforce construction of added edges
     for e in added_edges:
         fiberduct_update[e].lowBound = 1
 
+    # Enforce construction of pairs per span
     bidirectional_constraints = []
     for e in added_edges:
         constr = fiberduct_update[(e[1],e[0])] == fiberduct_update[e]
@@ -59,8 +69,11 @@ def find_capacity_update(alpha, topology, paths, demand, scenarios, fiberduct_ca
     ##    for each flow: each rel. scenario    ##
     ##    active_allocation >= demand    ##
     #############################################
-    flow2path = dict(zip(flows, [[p for p in range(n_paths) if f[0]==paths[p][0][0] and f[1]==paths[p][-1][1]] for f in flows]))
-    satisfaction_constraints = [lpSum([allocations[path] for path in flow2path[f] if set(paths[path]).isdisjoint(set(s))]) >= managed[flat_scenarios.index(sorted(s))]*demand[f] for f,scens in scenarios for s,_ in scens]
+    flow2path = dict(zip(flows, [[p for p in range(n_paths)
+        if f[0]==paths[p][0][0] and f[1]==paths[p][-1][1]] for f in flows]))
+    satisfaction_constraints = [lpSum([allocations[path] for path in flow2path[f]
+        if set(paths[path]).isdisjoint(set(s))]) >= managed[flat_scenarios.index(sorted(s))]*demand[f]
+        for f,scens in scenarios for s,_ in scens]
 
     #############################################
     ##    CAPACITY CONSTRAINTS                 ##
@@ -68,8 +81,11 @@ def find_capacity_update(alpha, topology, paths, demand, scenarios, fiberduct_ca
     ##    sum_allocations_edge <= c + c_up     ##
     #############################################
     # map edges to paths
-    edge2path = dict(zip(edges,[[p for p in range(n_paths) if e in paths[p]] for e in edges]))
-    capacity_constraints = [lpSum([allocations[ind] for ind in edge2path[e]]) <= capacity[e]+capacity_update[e] for e in edges]
+    edge2path = dict(zip(edges,[[p for p in range(n_paths)
+        if e in paths[p]] for e in edges]))
+    capacity_constraints = [lpSum([allocations[ind]
+        for ind in edge2path[e]]) <= capacity[e]+capacity_update[e]
+        for e in edges]
 
     #############################################
     ##   MAXIMUM CAPACITY UPDATE CONSTRAINTS   ##
@@ -78,6 +94,7 @@ def find_capacity_update(alpha, topology, paths, demand, scenarios, fiberduct_ca
     #############################################
     max_capacity_constraints = [capacity[e] + capacity_update[e] <= num_fiberducts[e]*fiberduct_capacity +fiberduct_update[e] * fiberduct_capacity for e in edges]
 
+    # Minimize the cost of upgrades
     objective = lpSum([capacity_update[e] * gbps_cost + fiberduct_update[e] * (fiberduct_cost/2) for e in edges])
 
     for a in availability_constraints:
@@ -89,19 +106,14 @@ def find_capacity_update(alpha, topology, paths, demand, scenarios, fiberduct_ca
     for a in max_capacity_constraints:
         prob += a
 
-    if cheapest != None:
-        prob += lpSum([capacity_update[e] * gbps_cost + fiberduct_update[e] * fiberduct_cost for e in edges]) <= cheapest
-
     prob += objective
 
     solver = GUROBI(msg=0)
     prob.solve(solver)
 
     if prob.status == LpStatusOptimal:
-        #print(objective.value())
-        return objective.value(), {k: capacity_update[k].value() for k in capacity_update}, {k: fiberduct_update[k].value() for k in fiberduct_update}
+        return objective.value(), {k: capacity_update[k].value()
+                for k in capacity_update}, {k: fiberduct_update[k].value()
+                        for k in fiberduct_update}
     else:
-        #print('heeeeeeeeeeeee')
-        #print(float("inf"))
         return float("inf"), None, None
-        raise Exception('Not Implemented')
